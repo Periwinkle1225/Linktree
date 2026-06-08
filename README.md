a3fe08d579a1b8bbd6e6f1f40c87532aec7a852c
Ricardo Losoya <ricardolosoya1@gmail.com>
2026-06-07 22:13:55 -0700

    Added dark toggle and url

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 Linktree-style page in React + css using vite for Associate
 Technical Assessment. First time using React and vite before.
 
-Why I built it this way: While following along The Odin Project I learned HTML + CSS and a bit of javascript. I was already planning on learning react with The Odin Project. Vite was also very new to me but following along on the Vite documentation made the setup for this easy.
+Why I built it this way: While following along The Odin Project I learned HTML + CSS and a bit of Javascript. I had zero experience with React before this but I was already planning on learning it with The Odin Project. Vite was also very new to me but following along on the Vite documentation made the setup for this easy.
 
 Resources used:
 React: https://react.dev/learn
 Vite: https://vite.dev/guide/#scaffolding-your-first-vite-project
-Tutorial for Dark/light toggle: https://www.youtube.com/watch?v=sy-rRtT84CQ
+Dark/light toggle: https://www.youtube.com/watch?v=sy-rRtT84CQ
+
+
+If given more time: I would definetely clean up my code. Its a bit messy but overall readable.
+
+Tricky: Im very new to Javascript so building the toggle was very difficult. I couldnt manage to change the text when the toggle happens it just says darkmode the whole time. I planned on having the resume pop out on the page when hovering over the resume div but couldnt manage to do it without having the resume div be too big.
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import './App.css'
 import { Toggle } from './components/Toggle';
 import { useLocalStorage } from "usehooks-ts"; //From use-local-storage didnt work for me so i had to use
@@ -6,33 +5,45 @@ import profile from "./assets/Profile.jpg";
 import linkedIn from "./assets/linkedin-icon-free.png"
 import github from "./assets/github.png";
 import resume from "./assets/resume.png"
+import { useEffect } from "react";
 
 const user = {
   name: 'Ricardo Losoya',
-  about: 'Computer science student',
+  about: 'Computer science student cross enrolled at Hartnell college and Csumb',
+  location: 'Salinas, CA',
+  pNumber: '(831) 905-7140',
+  email: 'ricardolosoya1@gmail.com'
 };
 
 const images = {
   imagePath: profile,
   imageSize: 90,
+  imageSize2: 70,
   linkedInImage: linkedIn,
   githubImage: github,
-  resumeImage: resume
+  resumeImage: resume,
 };
 
 const links = {
   linkedInLink: 'https://www.linkedin.com/in/ricardo-losoya-478507280/',
-  githubLink: 'https://github.com/Periwinkle1225'
+  githubLink: 'https://github.com/Periwinkle1225',
+  resumeLink: 'https://drive.google.com/file/d/1quBznAl6ck9pgIz1HGWVonS4ZLh-8SFP/view?usp=drive_link'
 };
+
 
 export default function MyApp() {
   const [isDark, setIsDark] = useLocalStorage("isDark", false);
-  
+  useEffect(() => {
+    document.documentElement.setAttribute(
+      "data-theme",
+      isDark ? "dark" : "light"
+    );
+  }, [isDark]);
   return (
   <>
-  <div className="App" data-theme={isDark ? "dark" : "light"}>
-    <div className="header">
+  <div className="App">
     <Toggle isChecked={isDark} handleChange={() => setIsDark(!isDark)}/>
+    <div className="header">
     </div>
     <div className="profile">
       <img
@@ -45,39 +56,62 @@ export default function MyApp() {
         }}
         />
         <h1>{user.name}</h1>
-        <p>{user.about}</p>
+        <p className="about">{user.about}</p>
+        <p className="location">{user.location}</p>
       </div>
 
       <div className="links">
-        <div>
+        <a href={links.linkedInLink}>
+        <div className="links2">
+        <div className="container">
         <img src={images.linkedInImage} 
         style={{
-          width: images.imageSize,
-          height: images.imageSize
+          width: images.imageSize2,
+          height: images.imageSize2
         }}
         />
-        <h1><a href={links.linkedInLink}>Check out my LinkedIn profile!!!</a></h1>
+        <h1>Check out my LinkedIn!!!</h1>
         </div>
-        <div>
+        <div className="url">
+        <p>{links.linkedInLink}</p>
+        </div>
+        </div></a>
+        <a href={links.githubLink}>
+        <div className="links2">
+        <div className="container">
         <img src={images.githubImage} 
         style={{
-          width: images.imageSize,
-          height: images.imageSize
+          width: images.imageSize2,
+          height: images.imageSize2
         }}
         />
-        <h1><a href={links.githubLink}>Check out my GitHub profile!!!</a></h1>
+        <h1>Check out my Github!!!</h1>
         </div>
-        <div>
+        <div className="url">
+        <p>{links.githubLink}</p>
+        </div>
+        </div></a>
+        <a href={links.resumeLink}>
+        <div className="links2">
+        <div className="container">
         <img src={images.resumeImage} 
         style={{
-          width: images.imageSize,
-          height: images.imageSize
+          width: images.imageSize2,
+          height: images.imageSize2
         }}
         />
-        <h1></h1>
+        <h1>Check out my Resume!!!</h1>
         </div>
+        <div className="url">
+        <p>{links.resumeLink}</p>
+        </div>
+        </div></a>
       </div>
-    </div>
+      <div className="footer">
+        <p>{user.pNumber}</p>
+        <p>{user.email}</p>
+      </div>
+  </div>
   </>
   );
 }
